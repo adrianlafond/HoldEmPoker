7158049640420dfbed771e3ae4ad167fa398fd17
adrian <adrian@disbranded.com>
2013-10-03 23:27:10 -0400

    remove private decks object

--- spec/deckSpec.js
+++ spec/deckSpec.js
@@ -1,32 +1,33 @@
 'use strict'
 
 
-describe('Poker.Deck', function () {  
+describe('Poker.Deck', function () {
   var Deck = DISBRANDED.Poker.Deck
-  
+
   it('should be a proper deck', function () {
     var deck = new Deck
     expect(deck.count()).toBe(52)
     deck.shuffle()
-    
+
     deck.deal()
     deck.deal()
     deck.deal()
     deck.deal()
     deck.deal()
     expect(deck.cards().length).toBe(47)
-    
+
     deck.deal()
     deck.deal()
     deck.deal()
     deck.deal()
     deck.deal()
     expect(deck.cards().length).toBe(42)
-    expect(deck.jokers()).toBe(0)
-    
+
+    expect(deck.jokers).toBe(0)
+
     deck.reset()
     expect(deck.deal()).toBe('AS')
-
+    
     deck.shuffle()
     expect(deck.addJokers(2).count()).toBe(54)
     expect(deck.removeJokers().count()).toBe(52)
@@ -39,10 +40,10 @@ describe('Poker.Deck', function () {
     expect(deck.addJokers(-2).count()).toBe(54)
     expect(deck.addJokers(1.88445632).count()).toBe(55)
   })
-  
+
   it('should add jokers at instantiation', function () {
     var deck = new Deck({ jokers: 2 })
-    expect(deck.jokers()).toBe(2)
+    expect(deck.jokers).toBe(2)
     expect(deck.count()).toBe(54)
   })
 })
--- src/deck.js
+++ src/deck.js
@@ -3,100 +3,67 @@
  */
 ;(function () {
   'use strict'
-  
-
-    /**
-     * Store instances with unique IDs.
-     */
-    var decks = {},
-        uid = 0,
 
 
-        /**
-         * Private constants.
-         */
-        CARDS = [
-          '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
-          '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD',
-          '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
-          '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS'
-        ],
-  
-        SIZE = CARDS.length,
 
-        MIN_JOKERS = 0,
-        MAX_JOKERS = 4
-      
-      
-  
-  function getUid() {
-    return 'deck-' + uid++
-  }
-  
-  
+  /**
+   * Private constants.
+   */
+  var CARDS = [
+        '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'JC', 'QC', 'KC', 'AC',
+        '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD',
+        '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'JH', 'QH', 'KH', 'AH',
+        '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'JS', 'QS', 'KS', 'AS'
+      ],
+
+      SIZE = CARDS.length,
+
+      MAX_JOKERS = 4
+
+
 
   Deck = function (options) {
-    var deck
-    
-    this.key = getUid()
-    deck = decks[this.key] = {
-      instance: this,
-      cards: CARDS.slice(0),
-      cardsIn: [],
-      cardsOut: [],
-      jokers: 0,
-      shuffled: false
-    }
-    
+    this.cardsAll = CARDS.slice(0)
+    this.cardsIn = []
+    this.cardsOut = []
+    this.shuffled = false
+    this.jokers = 0
+
     if (typeof options === 'object') {
       this.addJokers(parseInt(options.hasOwnProperty('jokers') ? options.jokers : 0, 10))
     }
     this.reset()
   }
-  
-  
-  
-  
+
+
+
+
   Deck.prototype = {
-    
+
     /**
      * @returns undealt cards remaining in deck.
      */
     cards: function () {
-      return decks[this.key].cardsIn.slice(0)
+      return this.cardsIn.slice(0)
     },
-    
-    /**
-     * @returns number of jokers in the deck.
-     */
-    jokers: function () {
-      return decks[this.key].jokers
-    },
-    
+
     /**
      * @returns number of cards in the deck.
      */
     count: function () {
-      return SIZE + this.jokers()
+      return SIZE + this.jokers
     },
-    
-    /**
-     * @returns {boolean}
-     */
-    shuffled: function () {
-      return decks[this.key].shuffled
-    },
-    
+
     /**
      * Remove all jokers from the deck.
      */
     removeJokers: function () {
-      decks[this.key].cards.slice(0, SIZE)
-      decks[this.key].jokers = 0
+      this.cardsAll.slice(0, SIZE)
+      this.jokers = 0
       this.reset()
       return this
     },
-    
+
     /**
      * Add up to 4 jokers to the deck.
      * @param {number} num Number of jokers to add.
@@ -104,50 +71,48 @@
     addJokers: function (num) {
       num = parseInt(num, 10)
       if (!isNaN(num)) {
-        num = Math.max(MIN_JOKERS, Math.min(num, MAX_JOKERS - decks[this.key].jokers))
+        num = Math.max(0, Math.min(num, MAX_JOKERS - this.jokers))
         _.times(num, function (n) {
-          decks[this.key].cards.push('W' + (n + 1))
+          this.cardsAll.push('W' + (n + 1))
         }, this)
-        decks[this.key].jokers += num
+        this.jokers += num
         this.reset()
       }
       return this
     },
-    
-    
+
+
     /**
      * Reset the deck to its original unshuffled order.
      */
     reset: function () {
-      var deck = decks[this.key]
-      deck.cardsIn = deck.cards.slice(0)
-      deck.cardsOut = []
-      deck.shuffled = false
+      this.cardsIn = this.cardsAll.slice(0)
+      this.cardsOut = []
+      this.shuffled = false
       return this
     },
-    
-    
+
+
     /**
      * Shuffle/randomize the deck.
      */
     shuffle: function () {
       this.reset()
-      decks[this.key].cardsIn = _.shuffle(decks[this.key].cardsIn)
-      decks[this.key].shuffled = true
+      this.cardsIn = _.shuffle(this.cardsIn)
+      this.shuffled = true
       return this
     },
-    
-    
+
+
     /**
      * Deal next card in the deck.
      * @returns the card dealt.
      */
     deal: function () {
-      var card = null,
-          deck = decks[this.key]
-      if (deck.cardsIn.length > 0) {
-        card = deck.cardsIn.pop()
-        deck.cardsOut.push(card)
+      var card = null
+      if (this.cardsIn.length > 0) {
+        card = this.cardsIn.pop()
+        this.cardsOut.push(card)
       }
       return card
     }
