a009939d8ca52ab954258fe7f50a5a09a44144bc
adrian <adrian@disbranded.com>
2013-10-12 23:54:17 -0400

    remove all dependencies from lingo.en

--- src/lingo.en.js
+++ src/lingo.en.js
@@ -5,20 +5,20 @@
    */
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['underscore'], factory)
+    define(factory)
   } else if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like enviroments that support module.exports,
     // like Node.
-    module.exports = factory(require('underscore'))
+    module.exports = factory()
   } else {
     // Browser globals (root is window)
     root.DISBRANDED = root.DISBRANDED || {}
     root.DISBRANDED.Poker = root.DISBRANDED.Poker || {}
     root.DISBRANDED.Poker.lingo = root.DISBRANDED.Poker.lingo || {}
-    root.DISBRANDED.Poker.lingo.en = factory(root._)
+    root.DISBRANDED.Poker.lingo.en = factory()
   }
-}(this, function (_) {
+}(this, function () {
   'use strict'
 
   return {
