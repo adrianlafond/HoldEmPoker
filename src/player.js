b24c4c3f281c9d654004360deabc39ab46c2ec6e
adrian <adrian@disbranded.com>
2013-10-14 22:09:29 -0400

    add constants and other basics to Player

--- src/player.js
+++ src/player.js
@@ -15,7 +15,10 @@
 
   defaults = {
     //
-  }
+  },
+
+  UP    = 'up',
+  DOWN  = 'down'
 
 
   /**
@@ -23,11 +26,39 @@
    */
   Player = function (options) {
     this.options = util.extend({ id: uid() }, defaults, options || {})
+    this.folded = false
+    this.chips = 0
+    this.bet = 0
+    this.hand = new Hand
+    this.cardsUp = []
+    this.cardsDn = []
   }
 
   Player.prototype = {
-    //
+
+    /**
+     *
+     */
+    addCard: function (options) {
+      this.hand.add(options.card)
+      if (options.face = UP) {
+        this.cardsUp.push(options.card)
+      } else {
+        this.cardsDn.push(options.card)
+      }
+    },
+
+    removeCards: function () {
+      //
+    }
   }
+
+
+  // Constants that correspond with lingo[lang].action array.
+  Player.FOLD   = 1
+  Player.BET    = 2
+  Player.CALL   = 3
+  Player.RAISE  = 4
 }());
 
 
