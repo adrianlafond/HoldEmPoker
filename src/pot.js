c625cdf8ca9e2c14e7adb21dff9247afa1908f0e
adrian <adrian@disbranded.com>
2013-10-16 22:59:11 -0400

    add pot constants

--- src/pot.js
+++ src/pot.js
@@ -20,6 +20,14 @@
   Pot.prototype = {
     //
   }
+
+
+  // Contstants correspond to lingo[lang].pot.
+  Pot.FIXED_LIMIT     = 0
+  Pot.SPREAD_LIMIT    = 1
+  Pot.POT_LIMIT       = 2
+  Pot.NO_LIMIT        = 3
+  Pot.CAP_LIMIT       = 4
 }());
 
 
