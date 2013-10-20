1d628812b0130246f663a400c8e11adc6a13aaf5
adrian <adrian@disbranded.com>
2013-10-20 11:10:28 -0400

    add stub pot spec test file

---
+++ spec/potSpec.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+describe('Poker.Pot', function () {
+  var Poker = DISBRANDED.Poker,
+      Pot = Poker.Pot,
+      SidePot = Poker.SidePot,
+      Round = Poker.Round,
+      Bet = Poker.Bet
+
+  it('should...', function () {
+
+  })
+})
+
